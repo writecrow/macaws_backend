56cb13768a8408e135d7ffb94f4f7dbd58d721df
markfullmer <mfullmer@gmail.com>
2018-07-08 15:01:31 -0500

    Support lemma-based searching on individual texts

--- profiles/corpus/modules/custom/corpus_api_texts/src/Plugin/views/field/ConcordanceViewsField.php
+++ profiles/corpus/modules/custom/corpus_api_texts/src/Plugin/views/field/ConcordanceViewsField.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\corpus_api_texts\Plugin\views\field;
 
+use Drupal\Component\Utility\Unicode;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\views\Plugin\views\field\FieldPluginBase;
 use Drupal\views\ResultRow;
@@ -58,15 +59,79 @@ class ConcordanceViewsField extends FieldPluginBase {
     $text = $text_object[0]['value'];
     $output = '';
     if (isset($param['search'])) {
+      if (isset($param['method']) && $param['method'] == 'lemma') {
+        $output .= "(Showing lemmatized matches)";
+        $all_words = preg_split("/[^\w]+/", $param['search'], 0, PREG_SPLIT_NO_EMPTY);
+        $keywords = $this->getLemmas($param['search']);
+        $param['search'] = implode(' ', array_keys($keywords));
+      }
       preg_match_all("/\"([^\"]+)\"/u", $param['search'], $phrases);
       $excerpt = Kwic::excerpt($text, $param['search']);
-      $output .= '<table>';
-      foreach ($excerpt as $line) {
-        $output .= '<tr><td>' . $line . '</td></tr>';
+      if (!empty($excerpt)) {
+        $output .= '<table>';
+        foreach ($excerpt as $line) {
+          $output .= '<tr><td>' . $line . '</td></tr>';
+        }
+        $output .= '</table>';
+        return $output;
       }
-      $output .= '</table>';
     }
-    return $output;
+    return '';
+  }
+
+  /**
+   * Extracts the positive keywords used in a search query.
+   *
+   * @param \Drupal\search_api\Query\QueryInterface $query
+   *   The query from which to extract the keywords.
+   *
+   * @return string[]
+   *   An array of all unique positive keywords used in the query.
+   */
+  protected function getLemmas($string) {
+    $module_handler = \Drupal::service('module_handler');
+    $this->module_path = $module_handler->getModule('search_api_lemma')->getPath();
+    $split = '/[' . Unicode::PREG_CLASS_WORD_BOUNDARY . ']+/iu';
+    $keywords_in = preg_split($split, $string);
+    // Assure there are no duplicates. (This is actually faster than
+    // array_unique() by a factor of 3 to 4.)
+    // Remove quotes from keywords.
+
+    $lemmatized = [];
+    foreach (array_filter($keywords_in) as $word) {
+      $alpha = $word[0];
+      $path = DRUPAL_ROOT . '/' . $this->module_path . '/data/lemmas_' . $alpha . '.php';
+      if (file_exists($path)) {
+        require $path;
+      }
+      if (isset($lemma_map[$word])) {
+        $lemma = $lemma_map[$word];
+      }
+      else {
+        $lemma = $word;
+      }
+      $lemmatized[$lemma] = $lemma;
+    }
+    
+    $keywords = [];
+    foreach (array_filter($lemmatized) as $keyword) {
+      if ($keyword = trim($keyword, "'\"")) {
+        $keywords[$keyword] = $keyword;
+      }
+      $alpha = $keyword[0];
+      $path = DRUPAL_ROOT . '/' . $this->module_path . '/data/roots_' . $alpha . '.php';
+      if (file_exists($path)) {
+        require $path;
+      }
+      if (isset($root_map[$keyword])) {
+        $lemmas = explode(',', $root_map[$keyword]);
+        foreach ($lemmas as $lemma) {
+          $keywords[$lemma] = $lemma;
+        }
+      }
+    }
+
+    return $keywords;
   }
 
 }
