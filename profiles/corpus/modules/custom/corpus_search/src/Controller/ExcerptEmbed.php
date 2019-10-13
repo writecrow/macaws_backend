ec48ffe426ff7dc0961a545f20d4ae82d96ef1bc
markfullmer <mfullmer@gmail.com>
2019-10-13 18:53:39 -0500

    Allow for sort on excerpt embed

--- profiles/corpus/modules/custom/corpus_search/src/Controller/ExcerptEmbed.php
+++ profiles/corpus/modules/custom/corpus_search/src/Controller/ExcerptEmbed.php
@@ -22,8 +22,27 @@ class ExcerptEmbed extends CorpusSearch {
     // Response.
     $results = self::getSearchResults($request, $excerpt_type = "fixed");
     $response = new CacheableResponse('', 200);
+    $output = '';
     if (!empty($results['search_results'])) {
-      $output = "<style>
+      $inc = 0;
+      $lines = [];
+      foreach ($results['search_results'] as $result) {
+        if ($inc > 19) {
+          break;
+        }
+        $inc++;
+        preg_match('/<mark>([^<]*)<\/mark>(.[^a-zA-Z]*)/', $result['text'], $three);
+        $bookends = preg_split('/<mark>([^<]*)<\/mark>(.[^a-zA-Z]*)/', $result['text'], 2);
+        $start = $bookends[0];
+        $end = $bookends[1];
+        preg_match('/[^ ]*$/', trim($bookends[0]), $two);
+        $one = preg_split('/[^ ]*$/', trim($bookends[0]));
+        preg_match('/(\s*)([^\s]*)(.*)/', $end, $trailing);
+        $before = empty($two[0]) ? str_repeat("&nbsp;", 59) : $two[0];
+        $lines[] = [$one[0], $before, $three[0], $trailing[2], $trailing[3] . '<br>'];
+      }
+      $json_lines = json_encode($lines);
+      $output .= "<style>
         body {
           font-size: 14px;
           font-family: 'Lucida Console', Monaco, monospace;
@@ -43,16 +62,69 @@ class ExcerptEmbed extends CorpusSearch {
           background-color: #f5f5f5;
         }
       </style>";
-      $output .= '<table>';
-      $inc = 0;
-      foreach ($results['search_results'] as $result) {
-        if ($inc > 9) {
-          break;
+      $output .= '
+        <div id="concordance_lines"></div>
+
+        <script type="text/javascript">
+
+        // lines is a list of lists with 5 items each:
+        // 0: context before
+        // 1: word right before kwic
+        // 2: kwic
+        // 3: word right after kwic
+        // 4: context after
+
+        lines = ' . $json_lines . ' 
+
+        // comparator function to sort by word after the kwic
+        function comparator_after(a, b) {
+          first = a[3] === "" ? " " : a[3].toLowerCase();
+          second = b[3] === "" ? " " : b[3].toLowerCase();
+          if (first < second) return -1;
+          if (first > second) return 1;
+          return 0;
         }
-        $inc++;
-        $output .= "<tr><td>" . $result['text'] . '</td></tr>';
-      }
-      $output .= "</table>";
+
+        // comparator function to sort by word before the kwic
+        function comparator_before(a, b) {
+          first =  a[1] === "" ? " " : a[1].toLowerCase();
+          second = b[1] === "" ? " " : b[1].toLowerCase();
+          if (first < second) return -1;
+          if (first > second) return 1;
+          return 0;
+        }
+
+        // function to sort lines by the word after the kwic
+        function sort_after() {
+          sorted_lines = lines.sort(comparator_after);
+
+          const conc_line_div = document.getElementById("concordance_lines");
+          conc_line_div.innerHTML = "The lines below are sorted by the word right <strong>after</strong> the key word in context. <a href=\"#\" onclick=\"sort_before();return false;\">Sort by the word before</a>.<hr>";
+
+          for (const line of sorted_lines) {
+            conc_line_div.innerHTML += line.join(" ");
+          }
+
+        }
+
+        // function to sort lines by the word before the kwic
+        function sort_before() {
+          sorted_lines = lines.sort(comparator_before);
+
+          const conc_line_div = document.getElementById("concordance_lines");
+          conc_line_div.innerHTML = "The lines below are sorted by the word right <strong>before</strong> the key word in context. <a href=\"#\" onclick=\"sort_after();return false;\">Sort by the word after</a>.<hr>";
+
+          for (const line of sorted_lines) {
+            conc_line_div.innerHTML += line.join(" ");
+          }
+
+        }
+
+        // start with lines sorted by the word before the kwic
+        sort_before();
+
+
+        </script>';
       $response->setContent($output);
     }
     $response->getCacheableMetadata()->addCacheContexts(['url.query_args']);
