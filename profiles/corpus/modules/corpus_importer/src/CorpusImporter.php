cc85dc8c77168767910d1b788c2330546ed94985
Mark Fullmer <mfullmer@gmail.com>
2020-07-05 15:08:34 -0700

    Add full name to Macaws

--- profiles/corpus/modules/corpus_importer/src/CorpusImporter.php
+++ profiles/corpus/modules/corpus_importer/src/CorpusImporter.php
@@ -22,6 +22,10 @@ class CorpusImporter extends ImporterService {
     foreach ($vocabularies as $key => $vocabulary) {
       $taxonomies[$vocabulary->label()] = $vocabulary->id();
     }
+    $text['Target Language'] = 'Russian';
+    if ($text['Assignment Mode'] == 'Speech') {
+      $text['Assignment Mode'] = 'Speaking';
+    }
     foreach ($taxonomies as $name => $machine_name) {
       $tid = '';
       $save = TRUE;
