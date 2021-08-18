c72c6602355fc4b76d697d093401910655f732c4
Mark Fullmer <mfullmer@gmail.com>
2021-08-18 17:58:05 -0500

    40 - Support toggleable concordance numbers in excerpts (#41)

--- profiles/corpus/modules/custom/corpus_search/src/Controller/ExcerptEmbed.php
+++ profiles/corpus/modules/custom/corpus_search/src/Controller/ExcerptEmbed.php
@@ -4,6 +4,7 @@ namespace Drupal\corpus_search\Controller;
 
 use Drupal\Core\Cache\CacheableResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Drupal\Core\Render\Markup;
 
 /**
  * Corpus Search excerpt embed endpoint.
@@ -20,114 +21,101 @@ class ExcerptEmbed extends CorpusSearch {
    */
   public static function endpoint(Request $request) {
     // Response.
-    $results = self::getSearchResults($request, $excerpt_type = "fixed");
+    $module_handler = \Drupal::service('module_handler');
+    $module_path = $module_handler->getModule('corpus_search')->getPath();
     $response = new CacheableResponse('', 200);
-    $output = '';
+    $renderer = \Drupal::service('renderer');
+
+    $output = self::getResults($request);
+    $sorted_before_array = [
+      '#type' => 'table',
+      '#rows' => $output['sorted_before'],
+      '#header_columns' => 1,
+    ];
+    $sorted_after_array = [
+      '#type' => 'table',
+      '#rows' => $output['sorted_after'],
+      '#header_columns' => 1,
+    ];
+    $sorted_before = $renderer->renderPlain($sorted_before_array);
+    $sorted_after = $renderer->renderPlain($sorted_after_array);
+    $build = [
+      'page' => [
+        '#theme' => 'corpus_concordance',
+        '#sorted_before' => json_encode($sorted_before),
+        '#sorted_after' => json_encode($sorted_after),
+        '#css' => file_get_contents($module_path . '/css/concordance_lines.css'),
+        '#script' => file_get_contents($module_path . '/js/concordance_lines.js'),
+      ],
+    ];
+    $html = \Drupal::service('renderer')->renderRoot($build);
+    $response->setContent($html);
+    $response->getCacheableMetadata()->addCacheContexts(['url.query_args']);
+    return $response;
+  }
+
+  private static function getResults($request) {
+    $numbering = \Drupal::request()->query->get('numbering');
+    $results = self::getSearchResults($request, "fixed");
+    $output = [];
+    $table = [];
     if (!empty($results['search_results'])) {
       $inc = 0;
-      $lines = [];
       foreach ($results['search_results'] as $result) {
         if ($inc > 19) {
           break;
         }
         $inc++;
-        preg_match('/<mark>([^<]*)<\/mark>(.[^\w]*)/u', $result['text'], $three);
+        $table[$inc]['number'] = '';
+        // Put the highlighted keyword into an array.
+        preg_match('/<mark>([^<]*)<\/mark>(.[^\w]*)/u', $result['text'], $keyword_matches);
         $bookends = preg_split('/<mark>([^<]*)<\/mark>(.[^\w]*)/u', $result['text'], 2);
-        $start = $bookends[0];
-        $end = $bookends[1];
-        preg_match('/[^ ]*$/', trim($bookends[0]), $two);
-        $one = preg_split('/[^ ]*$/', trim($start));
-        preg_match('/(\s*)([^\s]*)(.*)/', $end, $trailing);
-        $before_length = mb_strlen($one[0] . $two[0]);
-        if ($before_length < 60) {
-          $makeup = 60 - $before_length;
-          $first = str_repeat("&nbsp;", $makeup) . $one[0];
+        preg_match('/[^ ]*$/', trim($bookends[0]), $preceding);
+        $before_split = preg_split('/[^ ]*$/', trim($bookends[0]));
+        $table[$inc]['start'] = $before_split[0] ?: '';
+        $table[$inc]['word_before'] = $preceding[0] ?: '';
+        $table[$inc]['keyword'] = $keyword_matches[0];
+        preg_match('/(\s*)([^\s]*)(.*)/', $bookends[1], $following);
+        $table[$inc]['after'] = $following[0] ?: '';
+
+        // Add spacing to visually make up for short start sections.
+        $before_length = mb_strlen($table[$inc]['word_before']);
+        $start_length = mb_strlen($table[$inc]['start']);
+        if (($before_length + $start_length) < 60) {
+          $makeup = 60 - ($before_length + $start_length);
+          $table[$inc]['start'] = str_repeat("&nbsp;", $makeup) . $table[$inc]['start'];
         }
-        else {
-          $first = $one[0];
-        }
-        $second = empty($two[0]) ? ' ' : $two[0];
-        $lines[] = [$first, $second, $three[0], $trailing[2], $trailing[3] . '<br>'];
       }
-      $json_lines = json_encode($lines);
-      $output .= "<style>
-        body {
-          font-size: 14px;
-          font-family: 'Lucida Console', Monaco, monospace;
-          width: 1200px;
-        }
-        #concordance_lines {
-          text-align: center;
-        }
-      </style>";
-      $output .= '
-        <div id="concordance_lines"></div>
+      usort($table, function ($a, $b) {
+        return mb_strtolower($a['word_before']) <=> mb_strtolower($b['word_before']);
+      });
+      $sorted = [];
+      $sorted['sorted_before'] = $table;
+      usort($table, function ($a, $b) {
+        return mb_strtolower($a['after']) <=> mb_strtolower($b['after']);
+      });
+      $sorted['sorted_after'] = $table;
 
-        <script type="text/javascript">
-
-        // lines is a list of lists with 5 items each:
-        // 0: context before
-        // 1: word right before kwic
-        // 2: kwic
-        // 3: word right after kwic
-        // 4: context after
-
-        lines = ' . $json_lines . ' 
-
-        // comparator function to sort by word after the kwic
-        function comparator_after(a, b) {
-          first = a[3] === "" ? " " : a[3].toLowerCase();
-          second = b[3] === "" ? " " : b[3].toLowerCase();
-          if (first < second) return -1;
-          if (first > second) return 1;
-          // The two strings are equal. Compare the final string...
-          first = a[4] === "" ? " " : a[4].trim().toLowerCase();
-          second = b[4] === "" ? " " : b[4].trim().toLowerCase();
-          if (first < second) return -1;
-          if (first > second) return 1;
-          return 0;
-        }
-
-        // comparator function to sort by word before the kwic
-        function comparator_before(a, b) {
-          first =  a[1] === "" ? " " : a[1].toLowerCase();
-          second = b[1] === "" ? " " : b[1].toLowerCase();
-          if (first < second) return -1;
-          if (first > second) return 1;
-          return 0;
-        }
-
-        // function to sort lines by the word after the kwic
-        function sort_after() {
-          sorted_lines = lines.sort(comparator_after);
-
-          const conc_line_div = document.getElementById("concordance_lines");
-          conc_line_div.innerHTML = "The lines below are sorted by the word right <strong>after</strong> the key word in context. <a href=\"#\" onclick=\"sort_before();return false;\">Sort by the word before</a>.<hr>";
-
-          for (const line of sorted_lines) {
-            conc_line_div.innerHTML += line.join(" ");
-          }
-
-        }
-
-        // function to sort lines by the word before the kwic
-        function sort_before() {
-          sorted_lines = lines.sort(comparator_before);
-
-          const conc_line_div = document.getElementById("concordance_lines");
-          conc_line_div.innerHTML = "The lines below are sorted by the word right <strong>before</strong> the key word in context. <a href=\"#\" onclick=\"sort_after();return false;\">Sort by the word after</a>.<hr>";
-
-          for (const line of sorted_lines) {
-            conc_line_div.innerHTML += line.join(" ");
-          }
-        }
-        // start with lines sorted by the word before the kwic
-        sort_before();
-        </script>';
-      $response->setContent($output);
+      $inc = 0;
+      foreach ($sorted['sorted_before'] as $key => $values) {
+        $inc++;
+        $number = $numbering == 1 ? $inc : '';
+        $output['sorted_before'][] = [
+          $number,
+          Markup::create($values['start'] . $values['word_before'] . '&nbsp;' . $values['keyword'] . $values['after']),
+        ];
+      }
+      $inc = count($sorted['sorted_after']);
+      foreach ($sorted['sorted_after'] as $key => $values) {
+        $number = $numbering == 1 ? $inc : '';
+        $output['sorted_after'][] = [
+          $number,
+          Markup::create($values['start'] . $values['word_before'] . '&nbsp;' . $values['keyword'] . $values['after']),
+        ];
+        $inc--;
+      }
     }
-    $response->getCacheableMetadata()->addCacheContexts(['url.query_args']);
-    return $response;
+    return $output;
   }
 
 }
