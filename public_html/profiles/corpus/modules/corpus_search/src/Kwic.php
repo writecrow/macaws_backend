6c1dbca49d9d8fd8dabe6240990054abe9fab0d6
markfullmer <mfullmer@gmail.com>
2023-06-27 18:29:48 -0700

    Change sentence output

--- public_html/profiles/corpus/modules/corpus_search/src/Kwic.php
+++ public_html/profiles/corpus/modules/corpus_search/src/Kwic.php
@@ -35,7 +35,7 @@ class Kwic {
       if (count($instances) >= $inc) {
         break;
       }
-      $sentence = Highlighter::process($sentence, array_keys($keywords), FALSE, "kwic");
+      $sentence = Highlighter::process($sentence, array_keys($keywords), FALSE, "sentence");
       if (mb_strpos($sentence, '<mark>') !== FALSE) {
         $instances[] = $sentence;
       }
